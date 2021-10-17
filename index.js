3715dfe4d425397d589345c8e5d67138573a6f03
GerryMG2 <00032515@uca.edu.sv>
2021-10-16 20:33:46 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+class blitz {
+    static send(){
+        console.log("hola");
+    }
+
+    constructor(){
+        this.url = "";
+        this.initialized = false;
+        
+    }
+
+    init()
+}
+
+
+
+const bt = new blitz();
+
+module.exports = bt
